d9414dbea00f3b0d803f849525dafd70eb040dfd
241048 <yuyuw.060303@gmail.com>
2025-12-15 22:55:08 +0900

    名前変更　渡邊

---
+++ dbcnect.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$user = "user";
+$pass = "user";        // XAMPP はパスワード空欄
+$dbname = "supermarketmanager";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    exit("DB接続エラー: " . $e->getMessage());
+}
+?>
