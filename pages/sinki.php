fa82513ee61538a20764b763238b2fe8355c4133
KATO JUN <c241006@ivy.ac.jp>
2026-01-14 11:45:23 +0900

    新規登録画面　微量編集　嘉藤

--- pages/sinki.php
+++ pages/sinki.php
@@ -51,11 +51,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SuperMarketMagament</title>
+    <title>SuperMarketManager</title>
     <link rel="stylesheet" href="../assets/css/sinki.css">
 </head>
 <body>
     <h1 class="title">新規登録</h1>
+    <form method="post">
     <div class="form-countainer">
         <div class="form-row">
             <label for="user_id">ID</label>
@@ -64,7 +65,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         <div class="form-row">
             <label for="password">パスワード</label>
-            <input type="password" id="password" name="password">            
+            <input type="password" id="password" name="password_hash">            
         </div>
     </div>
 
