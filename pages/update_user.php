0b1e0516e801ef167ecfd3ccef6f18f3ff747a6c
241045 <kuochijing33@gmail.com>
2026-03-11 11:26:29 +0900

    変更　村

--- pages/update_user.php
+++ pages/update_user.php
@@ -15,9 +15,29 @@ $id = $_POST['id'];
 $login_id = trim($_POST['login_id']);
 $role = $_POST['role'];
 
-// 更新処理
-$stmt = $pdo->prepare("UPDATE users SET login_id = ?, role = ? WHERE id = ?");
-$stmt->execute([$login_id, $role, $id]);
+$password = $_POST['password'];
+$password_confirm = $_POST['password_confirm'];
+
+/* パスワード確認 */
+if ($password !== $password_confirm) {
+    echo "<script>
+            alert('パスワードが一致しません');
+            history.back();
+          </script>";
+    exit;
+}
+
+/* パスワードハッシュ化 */
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+/* 更新処理 */
+$stmt = $pdo->prepare("
+UPDATE users
+SET login_id = ?, role = ?, password_hash = ?
+WHERE id = ?
+");
+
+$stmt->execute([$login_id, $role, $hash, $id]);
 
 header("Location: user_management.php");
 exit;
